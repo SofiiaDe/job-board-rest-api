f467c453f014471dd8f3e0f48e5d547479636a7f
SofiiaDe <sofiia.prydatko@gmail.com>
2023-05-03 18:50:58 +0300

    Initial Spring Boot project template

---
+++ src/main/java/com/restapi/jobboard/JobBoardRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/jobboard/JobBoardRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.jobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
